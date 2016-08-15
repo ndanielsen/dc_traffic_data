a105671c6874d5f781fbd593de32c6d61a803d8f
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-08-14 23:01:28 -0400

    added easy download scripts and updated readme

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# DC Parking Data Cleaning and Analysis
+# DC Parking Ticket and Moving Violations Data Cleaning and Analysis
 
 ## Set Up
 
@@ -9,3 +9,16 @@ Create a virtual environment with virtualenv.
 ### Install requirements
 
 `pip install -r requirements.txt`
+
+
+### Downloadable data
+
+`python setup.py <argument>`
+
+**Available arguments:**
+
+`raw` - Downloads raw data set folder 'vz-hackathon-2'
+`small_geojson` - geojson files and assorted dc geospatial data
+`parking_1million` - sample of 1 million parking violations in cleaned tsv format
+`parking_100k` - sample of 100k parking violations in cleaned tsv format
+`cleaned_parking_data` - full data set of 11,748,803 parking violations
--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ import requests
 
 from clint.textui import progress
 
-parking_file_zip = 'vz-hackathon+2.zip'
-data_folder = 'vz-hackathon'
-
+# commandline args
 raw = True if 'raw' in sys.argv else False
-clean = True if 'clean' in sys.argv else False
+# clean = True if 'clean' in sys.argv else False
 small_geojson = True if 'small_geojson' in sys.argv else False
+parking_1million = True if 'parking_1million' in sys.argv else False
+parking_100k = True if 'parking_100k' in sys.argv else False
+cleaned_parking_data = True if 'cleaned_parking_data' in sys.argv else False
 
-def download_unzip(url, datafile, data_folder):
+def download(url, datafile):
     if not os.path.isfile(datafile):
         print('Warning, this might take awhile dependin on your internet connection')
         with open(datafile, 'wb') as handle:
@@ -24,25 +25,58 @@ def download_unzip(url, datafile, data_folder):
             for block in progress.bar(response.iter_content(1024), expected_size=total_length):
                 handle.write(block)
                 handle.flush()
+
+def unzip(datafile, data_folder):
     if not os.path.isdir(data_folder):
         print('Unzipping %s File' % datafile)
         with zipfile.ZipFile(datafile,"r") as zip_ref:
             zip_ref.extractall()
+        print('Extracted to: %s', data_folder)
+
+def unzip_to_dir(datafile, data_folder):
+    print('Unzipping %s File' % datafile)
+    with zipfile.ZipFile(datafile,"r") as zip_ref:
+        zip_ref.extractall(data_folder)
+    print('Extracted to: %s', data_folder)
 
 if raw:
     url = 'https://s3.amazonaws.com/dctraffic/vz-hackathon+2.zip'
     file_zip = 'vz-hackathon+2.zip'
     data_folder = 'vz-hackathon'
-    download_unzip(url, file_zip, data_folder)
+    download(url, file_zip)
+    unzip(file_zip, data_folder)
 
-if clean:
-    url = 'https://s3.amazonaws.com/dctraffic/traffic.zip'
-    file_zip = 'traffic.zip'
-    data_folder = 'traffic'
-    download_unzip(url, file_zip, data_folder)
+# if clean:
+#     url = 'https://s3.amazonaws.com/dctraffic/traffic.zip'
+#     file_zip = 'traffic.zip'
+#     data_folder = 'traffic'
+#     download(url, file_zip)
+#     unzip(file_zip, data_folder)
 
 if small_geojson:
     url = 'https://s3.amazonaws.com/dctraffic/data.zip'
     file_zip = 'data.zip'
     data_folder = 'data'
-    download_unzip(url, file_zip, data_folder)
+    download(url, file_zip)
+    unzip(file_zip, data_folder)
+
+if parking_1million:
+    url = "https://s3.amazonaws.com/dctraffic/clean_parking_violations_1million_sample.csv.zip"
+    file_zip = "clean_parking_violations_1million_sample.csv.zip"
+    data_folder = 'sampled_data'
+    download(url, file_zip)
+    unzip_to_dir(file_zip, data_folder)
+
+if parking_100k:
+    url = "https://s3.amazonaws.com/dctraffic/clean_parking_violations_100k_sample.csv.zip"
+    file_zip = "clean_parking_violations_100k_sample.csv.zip"
+    data_folder = 'sampled_data'
+    download(url, file_zip)
+    unzip_to_dir(file_zip, data_folder)
+
+if cleaned_parking_data:
+    url = "https://s3.amazonaws.com/dctraffic/clean_parking_violations.csv.zip"
+    file_zip = "clean_parking_violations.csv.zip"
+    data_folder = 'cleaned_data'
+    download(url, file_zip)
+    unzip_to_dir(file_zip, data_folder)
