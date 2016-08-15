930e57228eb6cee444085440e1ef5e4ae76d9974
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-08-14 21:11:24 -0400

    added updates

---
+++ readme.md
@@ -0,0 +1,11 @@
+# DC Parking Data Cleaning and Analysis
+
+## Set Up
+
+Ensure that python3 is set up on your system.
+
+Create a virtual environment with virtualenv.
+
+### Install requirements
+
+`pip install -r requirements.txt`
--- setup.py
+++ setup.py
@@ -1,21 +1,48 @@
 import os
+import sys
 import zipfile
 import requests
 
+from clint.textui import progress
 
 parking_file_zip = 'vz-hackathon+2.zip'
+data_folder = 'vz-hackathon'
 
-if not os.path.isfile(parking_file_zip):
+raw = True if 'raw' in sys.argv else False
+clean = True if 'clean' in sys.argv else False
+small_geojson = True if 'small_geojson' in sys.argv else False
 
-    print('Warning, this might take awhile dependin on your internet connection')
+def download_unzip(url, datafile, data_folder):
+    if not os.path.isfile(datafile):
+        print('Warning, this might take awhile dependin on your internet connection')
+        with open(datafile, 'wb') as handle:
+            response = requests.get(url, stream=True)
+            if not response.ok:
+                print('ERROR')
+            print('Downloading %s' % datafile)
+            total_length = int(response.headers.get('content-length')) / 1024 + 1
+            for block in progress.bar(response.iter_content(1024), expected_size=total_length):
+                handle.write(block)
+                handle.flush()
+    if not os.path.isdir(data_folder):
+        print('Unzipping %s File' % datafile)
+        with zipfile.ZipFile(datafile,"r") as zip_ref:
+            zip_ref.extractall()
 
-    with open(parking_file_zip, 'wb') as handle:
-        response = requests.get('https://s3.amazonaws.com/dctraffic/vz-hackathon+2.zip', stream=True)
+if raw:
+    url = 'https://s3.amazonaws.com/dctraffic/vz-hackathon+2.zip'
+    file_zip = 'vz-hackathon+2.zip'
+    data_folder = 'vz-hackathon'
+    download_unzip(url, file_zip, data_folder)
 
-        if not response.ok:
-            # Something went wrong
-            print('ERROR')
+if clean:
+    url = 'https://s3.amazonaws.com/dctraffic/traffic.zip'
+    file_zip = 'traffic.zip'
+    data_folder = 'traffic'
+    download_unzip(url, file_zip, data_folder)
 
-        for block in response.iter_content(1024):
-
-            handle.write(block)
+if small_geojson:
+    url = 'https://s3.amazonaws.com/dctraffic/data.zip'
+    file_zip = 'data.zip'
+    data_folder = 'data'
+    download_unzip(url, file_zip, data_folder)
